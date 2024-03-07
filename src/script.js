7a0ba7860393e018609e435dff2e1f96bc152990
Szonja <szamosfalvi.szonja@ktch.hu>
2024-03-07 10:39:01 +0100

    hiba javitások

--- src/script.js
+++ src/script.js
@@ -7,3 +7,34 @@
 * Github: https://github.com/szonia/
 * Licenc: GNU GPL
 */
+
+const sideAInput = document.querySelector("#sideA");
+const sideBInput = document.querySelector("#sideB");
+const sideCInput = document.querySelector("#sideC");
+const sideDInput = document.querySelector("#sideD");
+const areaInput = document.querySelector("#area");
+const calcButton = document.querySelector("#calcButton");
+
+calcButton.addEventListener('click', () => {
+    startCalc();
+})
+
+function startCalc() {
+    let sideA = Number(sideAInput.value)
+    let sideB = Number(sideBInput.value)
+    let sideC = Number(sideCInput.value)
+    let sideD = Number(sideDInput.value)
+    let area = calcArea(sideA, sideB, sideC, sideD)
+    console.log("Area: ", area)
+    areaInput.value = area
+    
+}
+
+// area = ((s - sideA) * (s - sideB) * (s - sideC) * (s - sideD));
+
+function calcArea(sideA, sideB, sideC, sideD) {
+    // félkerület s kiszámitás
+    s = (sideA + sideB + sideC + sideD) / 2;
+    Math.sqrt((s - sideA) * (s - sideB) * (s - sideC) * (s - sideD));
+    return area;
+}
