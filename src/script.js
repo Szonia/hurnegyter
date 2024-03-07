7136ff5c32b5efa3648c43ff8d3757e8f8d67115
Szonja <szamosfalvi.szonja@ktch.hu>
2024-03-07 08:33:21 +0100

    Kezdés

---
+++ src/script.js
@@ -0,0 +1,9 @@
+/*
+* File: script.js
+* Author:  Szamosfalvi Szonja
+* Copyright: 2024,  Szamosfalvi Szonja
+* Group: Szoft I-1-N
+* Date: 2024-03-07
+* Github: https://github.com/szonia/
+* Licenc: GNU GPL
+*/
